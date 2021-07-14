adffc7135c66402d37ade21bdcd638d9cd5d0221
robertsayfullin <robertsayfullin@nbnco.com.au>
2021-07-14 10:31:16 +1000

    echo in post

--- tools/ci/plugins/github.sh
+++ tools/ci/plugins/github.sh
@@ -96,7 +96,8 @@ function post {
     if [[ ! -z $DATA ]]; then
         DATA="-H 'Content-Type: application/json' -d '$DATA'"
     fi
-    eval "curl -X POST -g -v -H 'Accept: application/vnd.github.v3+json' -H 'Authorization: token ${GITHUB_TOKEN}' ${GITHUB_URL}/${URL} -d ${DATA}"
+    echo "curl -X POST -g -H 'Accept: application/vnd.github.v3+json' -H 'Authorization: token ${GITHUB_TOKEN}' ${DATA} ${GITHUB_URL}/${URL}"
+    eval "curl -X POST -g -H 'Accept: application/vnd.github.v3+json' -H 'Authorization: token ${GITHUB_TOKEN}' ${DATA} ${GITHUB_URL}/${URL}"
 }
 
 ##
